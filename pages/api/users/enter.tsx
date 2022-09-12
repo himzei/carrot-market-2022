c0a153f6722c20e11962cacc580ba44d50cf732f
조현일 <himzei@gmail.com>
2022-09-12 20:14:54 +0900

    9.0

---
+++ pages/api/users/enter.tsx
@@ -0,0 +1,58 @@
+import client from "@libs/server/client";
+import withHandler from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) console.log("found it");
+  //   if (!user) {
+  //     console.log("Did not find. will create");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("found it");
+  //   if (!user) {
+  //     console.log("Did not find it. will create");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  // }
+  res.json({ ok: true });
+}
+
+export default withHandler("POST", handler);
